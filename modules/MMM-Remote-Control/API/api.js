d114af903b585bec18f416cd3f84884c94faecfb
LorenzBader <mr.lo.500@gmx.at>
2021-04-15 09:58:40 +0200

    getIP route implemented

--- modules/MMM-Remote-Control/API/api.js
+++ modules/MMM-Remote-Control/API/api.js
@@ -174,13 +174,13 @@ module.exports = {
 			fs.writeFile(
 				configPath,
 				header +
-					util.inspect(req.body.config, {
-						showHidden: false,
-						depth: null,
-						maxArrayLength: null,
-						compact: false
-					}) +
-					footer,
+				util.inspect(req.body.config, {
+					showHidden: false,
+					depth: null,
+					maxArrayLength: null,
+					compact: false
+				}) +
+				footer,
 				function (err) {
 					if (err) {
 						response = { success: false, status: "error", reason: "Could not save settings", info: error };
@@ -208,6 +208,35 @@ module.exports = {
 				self.answerGet({ data: "userPresence" }, req, res);
 			}
 		});
+
+		this.expressRouter.route("/getIP").get((req, res) => {
+			const { networkInterfaces } = require('os');
+
+			const nets = networkInterfaces();
+			const results = Object.create(null);
+
+			for (const name of Object.keys(nets)) {
+				for (const net of nets[name]) {
+					if (net.family === 'IPv4' && !net.internal) {
+						if (!results[name]) {
+							results[name] = [];
+						}
+						results[name].push(net.address);
+					}
+				}
+			}
+			if (results.length == 0) {
+				response = { success: false, status: "error", reason: "Could not read IPs", info: error };
+				status = 400;
+				res.status(status).json(response);
+				return console.log(err);
+			}
+			else {
+				let response = { success: true, data: results };
+				let status = 200;
+				res.status(status).json(response);
+			}
+		})
 
 		this.expressRouter.route("/update/:moduleName").get((req, res) => {
 			this.updateModule(req.params.moduleName, res);
